7159414ce62f280a51caf582d0c9abac1c133ae4
Ayushman Gupta <guptaayushman24@gmail.com>
2023-04-05 00:28:49 +0530

    Commit

--- app/src/main/java/com/mastercoding/fooddelivery/Cart.java
+++ app/src/main/java/com/mastercoding/fooddelivery/Cart.java
@@ -1,37 +1,70 @@
 package com.mastercoding.fooddelivery;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
 public class Cart extends AppCompatActivity {
 
+    Button button;
+
+    DBhelper db;
+    ArrayList<CartModel> cartList;
+    CartAdapter adapter;
+    CartAdapter adapter1;
+
+    RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cart2);
-//
-//        DBhelper dBhelper = new DBhelper(this);
-//
-//
-//
-//        int id;
-//        String name,price;
-//
-//        ArrayList<SouthModel> arrItems = dBhelper.fetchItem();
-//
-//        for (int i=0;i< arrItems.size();i++){
-//            Log.d("ADDCART","Name"+arrItems.get(i).name + " ,"+"Price"+arrItems.get(i).price);
-//        }
 
+        setContentView(R.layout.activity_cart);
+        db = new DBhelper(this);
+        cartList = new ArrayList<>();
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        button = findViewById(R.id.totalbutton);
+        recyclerView.setHasFixedSize(true);
 
+        viewData();
 
+        adapter1 = new CartAdapter(cartList);
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int totalPrice = adapter1.sumprice();
+                Toast.makeText(Cart.this, "Your total price is " + totalPrice, Toast.LENGTH_SHORT).show();
+                db.deleteAlldata();
+                cartList.clear();
 
-
+            }
+        });
 
     }
+
+    public void viewData() {
+        Cursor cursor = db.viewData();
+        if (cursor.getCount() == 0) {
+            Toast.makeText(this, "No items are added to the cart", Toast.LENGTH_SHORT).show();
+        } else {
+            while (cursor.moveToNext()) {
+                cartList.add(new CartModel(cursor.getString(1), cursor.getString(2)));
+            }
+            adapter = new CartAdapter(cartList);
+            recyclerView.setAdapter(adapter);
+            adapter.notifyDataSetChanged();
+        }
+    }
+
 }
